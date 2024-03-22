ebd331a51f5588309271b07d49e65aa516c75b54
gaon2002 <gaon2002@outlook.jp>
2024-03-22 11:04:37 +0900

    kadai_21の提出

---
+++ src/kadai_21/DictionaryExec_Chapter21.java
@@ -0,0 +1,30 @@
+package kadai_21;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		// 辞書クラスのインスタンスを作成
+		Dictionary_Chapter21 fruite = new Dictionary_Chapter21();
+		
+		//調べる英単語を配列にセットする
+		String[] check = {"apple","banana","grape","orange"};
+		
+		
+		//辞書を調べる
+		// 辞書の内容を出力するメソッド
+    	// 配列の内容をひとつずつ確認する
+        for ( int i = 0; i < check.length; i++) {
+        	
+        	fruite.printDictionary(check[i]);
+		
+        }
+
+	
+	
+	
+
+}
+
+}
+
+
---
+++ src/kadai_21/Dictionary_Chapter21.java
@@ -0,0 +1,44 @@
+package kadai_21;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+	
+		//クラスのフィールドとしてHashMapをインスタンス化
+		//辞書として機能するHashMapを宣言
+		private HashMap<String,String> dictionary = new HashMap<String,String>();
+		
+			//コンストラクタ内で辞書に要素を追加
+			//Mapを宣言すると同時に値を追加して初期化する
+			//辞書に10通りの英単語と意味を追加
+			public Dictionary_Chapter21() {
+				dictionary.put("apple", "りんご");
+				dictionary.put("peach", "桃");
+				dictionary.put("banana", "バナナ");
+				dictionary.put("lemon", "レモン");
+				dictionary.put("pear", "梨");
+				dictionary.put("kiwi", "キウイ");
+				dictionary.put("strawberry", "いちご");
+				dictionary.put("grape", "ぶどう");
+				dictionary.put("muscat", "マスカット");
+				dictionary.put("cherry", "さくらんぼ");
+			}
+			
+			//辞書を調べるメソッド。引数keyを設定して、DictionaryExec_Chapter21から内容を受け取る。
+		    public void printDictionary(String key) {
+		        
+		        //dictionaryの中にkeyが含まれていたら、意味を合わせて出力
+		    	//含まれていなければ、存在しないと出力
+		    	//containsKey()メソッドはjava.util.HashMapクラスに定義されている
+		        if (dictionary.containsKey(key)) {
+		           	System.out.println(key + "の意味は " + dictionary.get(key));
+		        }else{
+		        	System.out.println(key + "は辞書に存在しません");
+		        }
+		        	
+		            
+		        
+		    
+		    }
+
+}
